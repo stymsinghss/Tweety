633ffddd3fe6212dffb3f00b8bcf2d07a33d8ef1
stymsinghss <iamstymsingh@gmail.com>
2025-03-15 12:07:33 +0530

    Adding logic for registering user

--- internal/handler/user.go
+++ internal/handler/user.go
@@ -1,8 +1,42 @@
 package handler
 
-import "net/http"
+import (
+	"encoding/json"
+	"errors"
+	"github.com/stymsinghss/Tweety/internal/service"
+	"net/http"
+)
+
+type createUserInput struct {
+	Email, Username string
+}
 
 // register -> handles register user functionality
 func (h *handler) registerUser(w http.ResponseWriter, r *http.Request) {
-	return
+	var in createUserInput
+	defer r.Body.Close()
+
+	if err := json.NewDecoder(r.Body).Decode(&in); err != nil {
+		http.Error(w, err.Error(), http.StatusBadRequest)
+		return
+	}
+
+	err := h.CreateUser(r.Context(), in.Email, in.Username)
+	if errors.Is(err, service.ErrInvalidEmail) || errors.Is(err, service.ErrInvalidUsername) {
+		respondError(w, err, http.StatusUnprocessableEntity)
+		return
+	}
+
+	if errors.Is(err, service.ErrEmailTaken) || errors.Is(err, service.ErrUsernameTaken) {
+		respondError(w, err, http.StatusConflict)
+		return
+	}
+	
+	if err != nil {
+		respondInternalError(w, err)
+		return
+	}
+
+	w.WriteHeader(http.StatusNoContent)
+
 }
--- internal/service/user.go
+++ internal/service/user.go
@@ -1,8 +1,11 @@
 package service
 
 import (
+	"context"
 	"errors"
+	"fmt"
 	"regexp"
+	"strings"
 )
 
 var (
@@ -14,10 +17,55 @@ var (
 
 	// ErrInvalidEmail -> when email fails regex check
 	ErrInvalidEmail = errors.New("invalid email")
+
+	// rxUsername -> regular expression for username validation
+	rxUsername = regexp.MustCompile("^[a-zA-Z][[a-zA-Z0-9_-]{0,17}$")
+
+	// ErrInvalidUsername -> when username fails regex check
+	ErrInvalidUsername = errors.New("invalid username")
+
+	// ErrEmailTaken -> when email already exists
+	ErrEmailTaken = errors.New("email already exists")
+
+	// ErrUsernameTaken -> when username already exists
+	ErrUsernameTaken = errors.New("username already exists")
 )
 
 // User -> represents User
 type User struct {
 	ID int64 `json:"id"`
 	Username string `json:"username"`
+}
+
+// CreateUser -> inserts a user in database
+func (s *Service) CreateUser(ctx context.Context, email, username string) error {
+	// Validate email
+	email = strings.TrimSpace(email)
+	if !rxEmail.MatchString(email) {
+		return ErrInvalidEmail
+	}
+
+	// Validate username
+	username = strings.TrimSpace(username)
+	if !rxUsername.MatchString(username) {
+		return ErrInvalidUsername
+	}
+
+	// query
+	query := "INSERT INTO users (email, username) VALUES ($1, $2)"
+	_, err := s.db.ExecContext(ctx, query, email, username)
+
+	unique := IsUniqueViolation(err)
+	if unique && strings.Contains(err.Error(), "email") {
+		return ErrEmailTaken
+	}
+
+	if unique && strings.Contains(err.Error(), "username") {
+		return ErrUsernameTaken
+	}
+
+	if err != nil {
+		return fmt.Errorf("could not insert user: %v\n", err)
+	}
+	return nil
 }
