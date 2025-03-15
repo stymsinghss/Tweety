e2949b3a3340c0e17d693e21183ab6d88fe0ffa3
stymsinghss <iamstymsingh@gmail.com>
2025-03-15 09:18:58 +0530

    Adding Service layer

---
+++ internal/service/service.go
@@ -0,0 +1,20 @@
+package service
+
+import (
+	"database/sql"
+	"github.com/hako/branca"
+)
+
+// Service -> contains business logic
+type Service struct {
+	db *sql.DB
+	token *branca.Branca
+}
+
+// New -> returns an instance of Service
+func New(db *sql.DB, token *branca.Branca) *Service {
+	return &Service{
+		db,
+		token,
+	}
+}
